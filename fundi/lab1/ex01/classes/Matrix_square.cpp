4fc1b55df7cc405fb063b923de844c09472e1a4f
Dacia Nicki <dnicki@il-e3.msk.21-school.ru>
2021-02-18 22:14:28 +0300

    added matrix and tex_conv .h files

--- fundi/lab1/ex01/classes/Matrix_square.cpp
+++ fundi/lab1/ex01/classes/Matrix_square.cpp
@@ -1,17 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   TeX_convertible.h                                  :+:      :+:    :+:   */
+/*   Matrix_square.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dnicki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/18 20:53:44 by dnicki            #+#    #+#             */
-/*   Updated: 2021/02/18 21:04:30 by dnicki           ###   ########.fr       */
+/*   Created: 2021/02/18 21:19:34 by dnicki            #+#    #+#             */
+/*   Updated: 2021/02/18 22:07:32 by dnicki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-class Tex_convertible
+#include "Matrix_square.cpp"
+#include <stdlib.h>
+
+Matrix_square::Matrix_square()
 {
-public:
-	virtual std::string convert() const;
-};
+	this->arr = NULL;
+	this->size = 0;
+}
+
+Matrix_square::Matrix_square(Matrix_square matr)
+{
+	this->size = matr.size
+	if (size == 0)
+	{
+		this->arr = NULL;
+	}
+	else
+	{
+		this_arr = new 
+	}
+}
--- fundi/lab1/ex01/classes/Matrix_square.h
+++ fundi/lab1/ex01/classes/Matrix_square.h
@@ -1,17 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   TeX_convertible.h                                  :+:      :+:    :+:   */
+/*   Matrix.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dnicki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/18 20:53:44 by dnicki            #+#    #+#             */
-/*   Updated: 2021/02/18 21:04:30 by dnicki           ###   ########.fr       */
+/*   Created: 2021/02/18 21:03:51 by dnicki            #+#    #+#             */
+/*   Updated: 2021/02/18 22:13:42 by dnicki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-class Tex_convertible
+class Matrix_square
 {
+private:
+	double			**arr;
+	unsigned int	size;
 public:
-	virtual std::string convert() const;
+	Maxrix_square() const;
+	Matrix_square(Matrix_square matr);
+	Matrix_square(const double **arr, int size);
+	Matrix_square(int size, std::ifstream);
+	Matrix_square(std::ifstream);
+	Matrix_square(const std::initializer_list<int> &list);
 };
